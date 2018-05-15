14626f16a2a2f071f62aad9b2eabd811e1638e49
Dporn <Dporninfo@gmail.com>
2018-05-14 21:21:36 -0500

    Added config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+let config = {
+    port: 3030,
+    auth: {
+        client_id: 'dpornco.app',
+        redirect_uri: 'http://alpha003.dporn.co/auth/'
+    },
+    session: {
+        secret: ''
+    }
+};
+
+module.exports = config;
