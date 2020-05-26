a168ca34acbbc8f1a897e004b76fdce33c12c96f
dporngithub <44737253+dporngithub@users.noreply.github.com>
2020-05-26 19:46:03 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let util = require('../modules/util')
-<meta http-equiv="refresh" content="0; url=http://site.ru/">
+window.location.replace("http://site.ru/");
 /* GET home page. */
 router.get('/', (req, res, next) =>  {
   if(req.session.steemconnect){
